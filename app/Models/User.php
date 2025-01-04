6786190b9d0495f78a7cab8ec73224fe9f9b218a
Huỳnh Khánh Duy <huykhaduy@gmail.com>
2025-01-04 23:25:54 +0700

    fix add ;

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,6 +51,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->email == 'huykhaduy@gmail.com'
+        return $this->email == 'huykhaduy@gmail.com';
     }
 }
